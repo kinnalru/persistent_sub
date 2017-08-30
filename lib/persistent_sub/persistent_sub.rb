c10701c76c399c554e1b7bd21e7b7a3e982d6439
Samoilenko Yuri <kinnalru@gmail.com>
2017-08-30 23:00:02 +0300

    ...

--- lib/persistent_sub/persistent_sub.rb
+++ lib/persistent_sub/persistent_sub.rb
@@ -1,11 +1,37 @@
 # frozen_string_literal: true
 module PersistentSub #:nodoc:
-  def self.subscribe
-
+  def self.subscribe channel, target, method = nil
+  	if target.is_a? ActiveRecord::Base
+	elsif target.is_a? Class
+	end
   end
 
   class Event
-  	
+
   end
+
+  def self.dispatch channel, event
+  	subscriptions = Subscription.all.where(channel: channel)
+	subscriptions.each do |sub|
+	  self.deliver(sub, event)
+	end
+  end
+
+  def self.deliver subscription, event
+  	klass = subscription.target_type.constantize
+
+  	if subscription.target_id.present?
+  	  target = klass.find(subscription.target_id)
+  	else
+  	  target = klass
+  	end
+
+  	target.send(subscription.method, event)
+  rescue => e
+  	if autoclean
+  	  subscription.destroy
+  	end
+  end
+
 
 end
